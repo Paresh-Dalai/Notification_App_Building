6d66aefbdbac0fb5a0683cc71ebb4ff17c6daa97
paresh-dalai <amarjeetdalai17@gmail.com>
2023-07-01 10:47:12 +0530

    db_url_changed

--- configs/db.config.js
+++ configs/db.config.js
@@ -3,5 +3,6 @@
 module.exports = {
 
     DB_NAME : "notification_db",
-    DB_URL : "mongodb://127.0.0.1/notification_db"
+    // DB_URL : "mongodb://127.0.0.1/notification_db"
+    DB_URL : "mongodb+srv://Amarjeet_Dalai:Amarjeet123@crm-db.ohpqaiu.mongodb.net/"
 }
